f44206e0b05fe92e660542b0ff1e796eb8259361
bguerschom0 <bguerschom0@gmail.com>
2025-03-08 18:25:33 +0200

    Update departments.js

--- src/pages/background/departments.js
+++ src/pages/background/departments.js
@@ -1,15 +1,45 @@
 
 const departments = [
-  { id: 'dept_1', name: 'Information Technology' },
-  { id: 'dept_2', name: 'Human Resources' },
-  { id: 'dept_3', name: 'Finance & Accounting' },
-  { id: 'dept_4', name: 'Marketing & Communication' },
-  { id: 'dept_5', name: 'Operations' },
-  { id: 'dept_6', name: 'Research & Development' },
-  { id: 'dept_7', name: 'Customer Support' },
-  { id: 'dept_8', name: 'Sales' },
-  { id: 'dept_9', name: 'Legal' },
-  { id: 'dept_10', name: 'Administrative' }
+
+  { id: 'dept_1', name: 'MTNR CEO's Office' },
+  { id: 'dept_2', name: 'MTNR Consumer' },
+  { id: 'dept_3', name: 'MTNR Customer Operation & Customer Expaerience' },
+  { id: 'dept_4', name: 'MTNR Corporate Services' },
+  { id: 'dept_5', name: 'MTNR EBU' },
+  { id: 'dept_6', name: 'MTNR Finance' },
+  { id: 'dept_7', name: 'MTNR Human Resources' },
+  { id: 'dept_8', name: 'MTNR Internal Audit & Forencics' },
+  { id: 'dept_9', name: 'MTNR IT' },
+  { id: 'dept_10', name: 'MTNR Network' },
+  { id: 'dept_11', name: 'MTNR Risk & Compliance' },
+  { id: 'dept_12', name: 'MTNR Sales and Distribution' }
+
+
+  { id: 'dept_20', name: 'MMRL Business Analytics & Reporting' },
+  { id: 'dept_21', name: 'MMRL CEO's Office' },
+  { id: 'dept_22', name: 'MMRL Commercial' },
+  { id: 'dept_23', name: 'MMRL Finance' },
+  { id: 'dept_24', name: 'MMRL Internal Audit' },
+  { id: 'dept_25', name: 'MMRL Legal' },
+  { id: 'dept_26', name: 'MMRL Operation & Service Delivery' },
+  { id: 'dept_27', name: 'MMRL Product & Services' },
+  { id: 'dept_28', name: 'MMRL Risk & Compliance' },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 ];
 
 export default departments;
