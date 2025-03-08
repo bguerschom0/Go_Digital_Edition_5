7e20d6f40229e293964c67488adda9643fb5d7a7
bguerschom0 <bguerschom0@gmail.com>
2025-03-08 16:41:54 +0200

    Update SecurityGuardDashboard.jsx

--- src/pages/dashboard/SecurityGuardDashboard.jsx
+++ src/pages/dashboard/SecurityGuardDashboard.jsx
@@ -19,8 +19,6 @@ const SecurityGuardDashboard = () => {
     }
   };
 
-  const username = user?.full_name || user?.username;
-
   return (
     <div className="p-6">
       <div className="max-w-7xl mx-auto">
